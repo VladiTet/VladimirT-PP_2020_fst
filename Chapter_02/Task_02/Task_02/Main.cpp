e541b57de09ed8408f9c6d345a659643d53c4937
VladiTet <vtet03@gmail.com>
2020-12-03 12:51:45 +0200

    add task 02 #2

--- Chapter_02/Task_02/Task_02/Main.cpp
+++ Chapter_02/Task_02/Task_02/Main.cpp
@@ -7,11 +7,11 @@ int main()
 	cout << "Enter the radius of the circle: "<<endl;
 	cin >> radius;
 	const double Pi = 3.14;
+	
 	if (radius >= 0)
 	{
 		double S = radius * radius * Pi;
-		cout << "Area of the circle is: " << S;
-		
+		cout << "Area of the circle is: " << S;	
 	}
 	else
 	{
