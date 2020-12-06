3ca2b58291b2845d3f0b4105f0016560166a3ade
VladiTet <vtet03@gmail.com>
2020-12-06 19:44:14 +0200

    touching details task 05 #5

--- Chapter_02/Task_05/Task_05/Main.cpp
+++ Chapter_02/Task_05/Task_05/Main.cpp
@@ -7,11 +7,13 @@ using namespace std;
 int main() {
 
 	vector<string> v;
-	ifstream in("Fillvector.txt");
-	string line;
-	while (getline(in, line))
-		v.push_back(line); 
-	for (int i = v.size() - 1; i >= 0; i--)
-		cout << i << ": " << v[i] << endl;
-		system("pause");
+    ifstream in("Fillvector.txt");
+    string line;
+    while (getline(in, line))
+        v.push_back(line);
+    for (int i = v.size() - 1; i >= 0; i--)
+    {
+        cout << i << ": " << v[i] << endl;
+    }
+    system("pause");
 }
