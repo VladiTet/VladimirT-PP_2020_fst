7276ee3956b393655ce4ec33b97c1a01fd11b3be
VladiTet <vtet03@gmail.com>
2020-12-11 14:16:48 +0200

    add working task 10 #10

---
+++ Chapter_02/Task_10/Task_10/Main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void main()
+{
+    vector<float> a;
+
+    for (int i = 1; i <= 25; i++)
+    {
+          a.push_back(i * 2.14);
+    }
+
+    cout << "The original one: " << endl;
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i] << ",  ";
+    }
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        a[i] *= a[i];
+    }
+
+    cout << "\nAftermarket one: " << endl;
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i] << ",  ";
+    }
+
+    system("pause");
+}
